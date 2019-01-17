f7599e11f8dbab90661e555a9ffd8047d13a3687
Беднов Денис Игоревич <bednov_di@etsp.ru>
2019-01-17 10:10:45 +0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+# denisbednov.github.io
---
+++ scripts.js
@@ -0,0 +1,33 @@
+$(function () {
+    // $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function (e) {
+    //     if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+    //         e.preventDefault();
+
+    //         var a = $(this.hash);
+    //         if ((a = a.length ? a : $("[name=" + this.hash.slice(1) + "]")).length) {
+    //             return $("html, body").stop().animate({
+    //                 scrollTop: a.offset().top - 56
+    //             }, 1e3, "easeInOutExpo");
+    //         }
+    //     }
+    // });
+
+    /*$('#sendRequest').submit(function (e) {
+        e.preventDefault();
+        
+        var name = $('#sendRequestName').val(),
+            phone = $('#sendRequestPhone').val();
+        
+        Email.send(
+            "denisbednov@outlook.com",
+            "denisbednov@outlook.com",
+            "Miracode - Заявка на разработку сайта",
+            "Имя: " + name + ", телефон: " + phone,
+            "smtp.elasticemail.com",
+            "denisbednov@outlook.com",
+            "9ed9b957-5220-453b-bf1f-8ece16d9dc91",
+            function (message) {
+                alert('Ваш запрос отправлен. Спасибо за обращение.');
+        });
+    });*/
+});
